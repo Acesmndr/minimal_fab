3497d672670c0c4d26e2190d10e03713265639d3
acesmndr <acesmndr@gmail.com>
2016-03-24 14:23:11 +0545

    Shortened setup code and added UI enhancements

--- assets/fab.js
+++ assets/fab.js
@@ -1,36 +1,56 @@
-// v1.1.0
+// v1.2.0
 (function(){
+  let fabBg = document.createElement("div");
+  fabBg.className = "fab-bg";
+  fabBg.addEventListener("mouseout",(event) => {
+    event = event.toElement.classList||" ";
+    (event.contains("fab-child")||event.contains("fab-bg")||event.contains("fa"))?false:_closeFab();
+  });
+  document.body.appendChild(fabBg);
   fabSetup = (buttons) => {
-    let _fabTimer;
-    let fabBg = document.createElement("div");
-    fabBg.className = "fab-bg";
-    fabBg.addEventListener("mouseout",(event) => {
-      event = event.toElement.classList;
-      (event.contains("fab-main")||event.contains("fab-child")||event.contains("fab-bg")||event.contains("fa"))?false:_closeFab();
-    });
-    buttons.map((_btn)=>{
-      document.body.appendChild(fabBg);
-      _btn.icon = _btn.icon || "info";
-      _btn.help = _btn.help || "Instructions";
-      _btn.callback = _btn.callback || function(){};
+    for( let _btn in buttons ){
+      let _fabIcon,_fabHelp,_callback;
+      switch (_btn) {
+        case "instruction":
+          _fabIcon = "info";
+          _fabHelp = "Instructions";
+          _callback = ()=>{_bootboxAlert(`Instructions`,buttons[_btn])};
+          break;
+        case "reference":
+          _fabIcon = "file-text";
+          _fabHelp = "Reference";
+          _callback = ()=>{window.open(buttons[_btn],"_blank")};
+          break;
+        case "shortcut":
+          _fabIcon = "keyboard-o";
+          _fabHelp = "Shortcuts";
+          _callback = ()=>{_bootboxAlert(`Shortcuts`,buttons[_btn])};
+          break;
+        case "video":
+          _fabIcon = "video-camera";
+          _fabHelp = "Video";
+          _callback = ()=>{_bootboxAlert(`Training Video`,`<iframe width="800" height="600" src="`+buttons[_btn]+`" frameborder="0" allowfullscreen></iframe>`)};
+          break;
+        default:
+          _fabIcon = "bars";
+          _fabHelp = _btn;
+          _callback = buttons[_btn];
+      }
       let fabBtn = document.createElement("a");
       fabBtn.className = "fab-child";
-      fabBtn.id = "fab-"+_btn.icon;
       fabBtn.setAttribute("data-toggle","tooltip");
-      fabBtn.setAttribute("data-original-title",_btn.help);
       fabBtn.setAttribute("data-placement","left");
+      fabBtn.id = "fab-"+_fabIcon;
+      fabBtn.setAttribute("data-original-title",_fabHelp);
       fabBtn.addEventListener("click",function(){
+        _callback();
         _closeFab();
-        _btn.callback();
-      });
-      fabBtn.addEventListener("mouseover",function(){
-
       });
       let fabIcon = document.createElement("i");
-      fabIcon.className = "fa fa-fw fa-"+_btn.icon;
+      fabIcon.className = "fa fa-fw fa-"+_fabIcon;
       fabBtn.appendChild( fabIcon );
       fabBg.appendChild( fabBtn );
-    });
+    };
     let mainBtn = document.createElement("a");
     mainBtn.className = "fab-main";
     mainBtn.id = "fab";
@@ -49,7 +69,6 @@
       for( let i=0;i < document.getElementsByClassName("fab-child").length; i++ ){
           document.getElementsByClassName("fab-child")[i].classList.add("fab-child-display");
       }
-      // _fabTimeout = setTimeout(_closeFab,5000);
     }
     _closeFab = () => {
       mainBtn.setAttribute("data-original-title","Help");
@@ -57,16 +76,17 @@
       for( let i=0;i < document.getElementsByClassName("fab-child").length; i++ ){
         document.getElementsByClassName("fab-child")[i].classList.remove("fab-child-display");
       }
-      // clearTimeout(_fabTimeout);
+    };
+    _bootboxAlert = (title,message) => {
+      bootbox.dialog({
+        title: title,
+        message: message,
+        size: "large",
+        buttons: {ok: {label:"OK"}}
+      });
     };
     fabBg.appendChild( mainBtn );
     $('[data-toggle="tooltip"]').tooltip();
   };
-  fabSyntax = () => {
-    console.log(`fabSetup([{
-      icon:"info", // font-awesome icons eg. info for fa-info
-      help:"tooltip text",
-      callback:function(){}
-    }]); //insert multiple objects for multiple fab icons`);
-  };
+  fabSyntax = () => {console.log(`fabSetup({instruction:"instruction text",video:"video link",shortcut:"Shortcut text",reference:"Reference Link"});`);};
 })();
